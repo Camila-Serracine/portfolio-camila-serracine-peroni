84568c603778e0ee2379839948dbf9d7bff3acb4
camila.serracine <camilaserracinee@gmail.com>
2026-04-27 23:09:31 -0300

    Create Cliente and Veiculo tables in script.sql

---
+++ projeto-banco-dados/script.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Cliente (
+    id INT PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100)
+);
+
+CREATE TABLE Veiculo (
+    id INT PRIMARY KEY,
+    modelo VARCHAR(100),
+    marca VARCHAR(100)
+);
